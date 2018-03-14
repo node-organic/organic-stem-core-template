414c6ea55da1a507a56a9bf37ed2b40020bb5b1b
outbounder <obiwonn@gmail.com>
2018-03-14 16:37:47 +0200

    initial commit

---
+++ index.js
@@ -0,0 +1,8 @@
+const path = require('path')
+require('organic-stem-stack-upgrade').applyStackUpgrade({
+  sourceDir: path.join(__dirname, 'core'),
+  destDir: process.cwd()
+}).catch(err => {
+  console.error(err)
+  process.exit(1)
+})
