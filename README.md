57ccfe23085338af283446abe173811bb399d73b
outbounder <obiwonn@gmail.com>
2018-03-19 17:47:47 +0200

    update towards organic-stack-upgrade

---
+++ README.md
@@ -0,0 +1,11 @@
+# organic-stem-core-template
+
+a stem core template
+
+## usage
+
+```
+$ mkdir ./myStem
+$ cd ./myStem
+$ npx organic-stem-core-template
+```
--- index.js
+++ index.js
@@ -1,10 +1,25 @@
 #!/usr/bin/env node
 
+const StackUpgrade = require('organic-stack-upgrade')
 const path = require('path')
-require('organic-stem-stack-upgrade').applyStackUpgrade({
-  sourceDir: path.join(__dirname, 'core'),
-  destDir: process.cwd()
-}).catch(err => {
+const exec = require('util').promisify(require('child_process').exec)
+
+const execute = async function () {
+  let stack = new StackUpgrade({
+    destDir: process.cwd(),
+    name: 'organic-stem-core',
+    version: '1.0.0'
+  })
+  await stack.configureMergeAndUpdateJSON({
+    sourceDir: path.join(__dirname, 'core')
+  })
+  console.info('run npm install...')
+  let npmOutput = await exec('npm install')
+  console.info(npmOutput.stdout)
+  console.error(npmOutput.stderr)
+}
+
+execute().catch((err) => {
   console.error(err)
   process.exit(1)
 })
